6e346481fd37f62fe82bebafeb15eb8ca4ad0215
Francisco Esquivel <fesquive@mazda.com.mx>
2024-06-12 10:57:02 -0600

    Adjust 360 for multiple vehicle colors

--- src/utils/container-elements/create-360-view-icon.js
+++ src/utils/container-elements/create-360-view-icon.js
@@ -1,8 +1,10 @@
 export const create360ViewIcon = () => {
-  const view360Icon = document.createElement('div');
 
-  view360Icon.className = 'cloudimage-360-view-360-icon';
-  view360Icon.innerText = '0%';
+  const view360Icon = document.createElement('div');
+  view360Icon.className = 'mde-threehundredsixty__loader ajaxloader';
+  const inner = document.createElement('div');
+  inner.className = 'arc';
+  view360Icon.appendChild(inner);
 
   return view360Icon;
 };
